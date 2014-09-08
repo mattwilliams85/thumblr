f808ff24720d51a61d07c6dd60e8c9e13dd14e2b
Matthew <mattjunk85@yahoo.com>
2014-09-08 16:45:54 -0700

    fix naming error for user_id

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
 	def create
 		@user = User.find_by_email(params[:email])
 		if @user && @user.authenticate(params[:password])
-			session[:user_id] = @user.id
+			session[:users_id] = @user.id
 			redirect_to user_path(@user)
 		else 
 			render 'new'
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
 	end
 
 	def destroy
-		session[:user_id] = nil
+		session[:users_id] = nil
 		redirect_to root_path
 	end
 
