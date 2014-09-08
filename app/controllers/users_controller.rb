04209e1e0acf10ba9ed7dae1122de42a38ed1ad3
Matthew <mattjunk85@yahoo.com>
2014-09-08 15:40:17 -0700

    add login/logout feature with sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+	before_filter :logged_in?, only: [:new]
+	
 	def new
 		@user = User.new
 	end
@@ -6,10 +8,15 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
+			session[:user_id] = @user.id
 			redirect_to user_path(@user)
 		else 
 			render "new"
 		end
+	end
+
+	def show
+		@user = User.find(params[:id])
 	end
 
 private
