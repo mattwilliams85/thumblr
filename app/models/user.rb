b4c4469f31db2ad8d83520d76e6c1a1ee48d4fb6
Matthew <mattjunk85@yahoo.com>
2014-09-08 10:53:17 -0700

    add secure password validation with bcrypt gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
 	validates :password, :presence => true
 	validates_length_of :password, :maximum => 20
 	validates_length_of :password,  :minimum => 7
+	has_secure_password
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -4,4 +4,5 @@ describe User do
 	it { should validate_presence_of :password }
 	it { should ensure_length_of(:password).is_at_least(7) }
 	it { should ensure_length_of(:password).is_at_most(20) }
+	it { should have_secure_password }
 end
