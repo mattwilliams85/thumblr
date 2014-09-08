168c9c67fd740b15affc4b6f1256601aac827d1d
Matthew <mattjunk85@yahoo.com>
2014-09-08 10:46:41 -0700

    update migration for users

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20140908170358) do
 
   create_table "users", force: true do |t|
     t.string   "email"
-    t.string   "password"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
