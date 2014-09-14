1c617a5cd01c69c60163d5bc9c7da3f1e4538417
Matthew <mattjunk85@yahoo.com>
2014-09-14 12:26:55 -0700

    add new photo view, controller method, and model

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -7,7 +7,7 @@ describe User do
 	it { should validate_presence_of :password }
 	it { should ensure_length_of(:password).is_at_least(7) }
 	it { should ensure_length_of(:password).is_at_most(20) }
-	it { should validate_numericality_of(:password).
+	it { should validate_format_of(:password).
       with_message("must have at least one number") }	
 	it { should have_secure_password }
 	it { should have_many :articles }
